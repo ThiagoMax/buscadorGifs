a1dfbebb5b9f535ad9d963decc92f2f929a13ef4
ThiagoMax <thiagomaximiliano21@gmail.com>
2021-05-07 13:10:50 -0300

    Inserção de Navigator e botão Share

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -2,6 +2,9 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
+
+import 'gif_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -116,6 +119,17 @@ class _HomePageState extends State<HomePage> {
                   snapshot.data["data"][index]["images"]["fixed_height"]["url"],
                   height: 300.0,
                   fit: BoxFit.cover),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            GifPage(snapshot.data["data"][index])));
+              },
+              onLongPress: () {
+                Share.share(snapshot.data["data"][index]["images"]
+                    ["fixed_height"]["url"]);
+              },
             );
           else {
             return Container(
